0fbed4b3cdecc38629ab815d55ddfb4c39ec6b42
Sean Malloy <spinelli85@gmail.com>
2013-05-20 00:02:43 -0500

    Update main daemon.
    Many updates.

--- SPM-Util/lib/SPM/Daemon.pm
+++ SPM-Util/lib/SPM/Daemon.pm
@@ -4,9 +4,6 @@ use strict;
 use warnings;
 use English qw( -no_match_vars );
 
-# TODO: should init_server be broken out into
-#       two separate functions? ( init_server_root() and init_server_user() )
-
 use POSIX qw(setsid WNOHANG);
 use Carp 'croak', 'cluck';   # TODO: should expections be used instead?
 use File::Basename;
@@ -15,12 +12,26 @@ use Sys::Syslog qw(:DEFAULT setlogsock);
 require Exporter;
 
 our @ISA = qw(Exporter);
-our @EXPORT_OK = qw( init_server log_debug log_die log_notice log_warn );
+our @EXPORT_OK = qw( init_server_root init_server_user log_debug log_die log_notice log_warn );
 our $VERSION   = '0.01';
 
 use constant PIDPATH  => '/var/run';
 use constant FACILITY => 'local0';
 my ($Pid, $Pidfile);
+
+sub _become_daemon {
+    die "Can't fork" unless defined (my $child = fork);
+    exit 0 if $child;  # parent dies
+    setsid();          # become session leader
+    open(STDIN,  "</dev/null");
+    open(STDOUT, ">/dev/null");
+    open(STDERR, ">&STDOUT");
+    chdir '/';      # change working directory
+    umask(0);       # forget file mode creation mask
+    $ENV{PATH}  = '/bin:/sbin:/usr/bin:/usr/sbin';
+    $SIG{CHLD}  = '_reap_child';
+    return $PROCESS_ID;
+}
 
 sub _change_privilages {
     my $user  = shift;
@@ -32,62 +43,24 @@ sub _change_privilages {
     $EFFECTIVE_USER_ID  = $uid;    # change the effective UID (but the the real UID)
 }
 
-sub init_server {
-    $Pidfile  = shift;
-    my $user  = shift;
-    my $group = shift;
-    $Pidfile ||= _getpidfilename();
-    my $fh = _open_pid_file($Pidfile);
-    _become_daemon();
-    print $fh $PROCESS_ID;
-    close $fh;
-    _init_log();
-    if (defined $user && defined $group) {
-        _change_privilages($user, $group);
-    }
-    return $Pid = $PROCESS_ID;
-}
-
-sub _become_daemon {
-    die "Can't fork" unless defined (my $child = fork);
-    exit 0 if $child;  # parent dies
-    setsid();          # become session leader
-    open(STDIN,  "</dev/null");
-    open(STDOUT, ">/dev/null");
-    open(STDERR, ">&STDOUT");
-    chdir '/';      # change working directory
-    umask(0);       # forget file mode creation mask
-    $ENV{PATH} = '/bin:/sbin:/usr/bin:/usr/sbin';
-    $SIG{CHD}  = \&_reap_child;
-    return $PROCESS_ID;
-}
-
 sub _init_log {
     setlogsock('unix');
     my $basename = basename($PROGRAM_NAME);
     openlog($basename, 'pid', FACILITY);
-    $SIG{__WARN__} = \&log_warn;    # send warn() messages to syslog
-    $SIG{__DIE__}  = \&log_die;     # send die() messages to syslog
+    $SIG{__WARN__} = 'log_warn';    # send warn() messages to syslog
+    $SIG{__DIE__}  = 'log_die';     # send die() messages to syslog
 }
 
-sub log_die {
-    syslog('crit', _msg(@_));
-    die @_;
+sub _getpidfilename {
+    my $basename = basename($PROGRAM_NAME, '.pl');
+    return PIDPATH . "/$basename.pid";
 }
-sub log_debug  { syslog('debug',   _msg(@_)) }
-sub log_notice { syslog('notice',  _msg(@_)) }
-sub log_warn   { syslog('warning', _msg(@_)) }
 
 sub _msg {
     my $msg = join('', @_) || "Something's wrong";
     my ($pack, $filename, $line) = caller(1);
     $msg .= "$filename line $line\n" unless $msg =~ /\n$/;
     return $msg;
-}
-
-sub _getpidfilename {
-    my $basename = basename($PROGRAM_NAME, '.pl');
-    return PIDPATH . "/$basename.pid";
 }
 
 sub _open_pid_file {
@@ -107,9 +80,64 @@ sub _reap_child {
     do { } while waitpid(-1, WNOHANG) > 0;
 }
 
+# Return true is running as root.
+# Retrun false if not running as root.
+sub _running_as_root {
+    return(!$REAL_USER_ID);
+}
+
+sub init_server_root {
+    if (!_running_as_root()) {
+        die "must run as root!";
+    }
+    $Pidfile  = shift;
+    if (!defined $Pidfile) {
+        $Pidfile = _getpidfilename();
+    }
+    my $fh = _open_pid_file($Pidfile);
+    _become_daemon();
+    print $fh $PROCESS_ID;
+    close $fh;
+    _init_log();
+    return $Pid = $PROCESS_ID;
+}
+
+sub init_server_user {
+    if (!_running_as_root()) {
+        die "must run as root!";
+    }
+    $Pidfile  = shift;
+    my $user  = shift;
+    my $group = shift;
+    if (!defined $user && !defined $group) {
+        die "cannot drop privilages, missing user or group parameter!";
+    }
+    if (!defined $Pidfile) {
+        $Pidfile = _getpidfilename();
+    }
+    my $fh = _open_pid_file($Pidfile);
+    _become_daemon();
+    print $fh $PROCESS_ID;
+    close $fh;
+    _init_log();
+    _change_privilages($user, $group);
+    return $Pid = $PROCESS_ID;
+}
+
+sub log_die {
+    syslog('crit', _msg(@_));
+    die @_;
+}
+sub log_debug  { syslog('debug',   _msg(@_)) }
+sub log_notice { syslog('notice',  _msg(@_)) }
+sub log_warn   { syslog('warning', _msg(@_)) }
+
 END { 
-    $EFFECTIVE_USER_ID = $REAL_USER_ID;     # regain privilages
-    unlink $Pidfile if defined $Pid and $$ = $Pid;
+    $EFFECTIVE_USER_ID = $REAL_USER_ID;     # regain root privilages
+    if (defined $Pid && $Pid == $PID) {
+        log_notice("removing pidfile $Pidfile", "\n");
+        unlink $Pidfile;
+    }
 }
 
 1;
@@ -134,12 +162,15 @@ be explicitly requested.
 
 =head1 FUNCTIONS
 
-=head2 init_server(PIDFILE, USER, GROUP)
+=head2 init_server_root(PIDFILE)
 
-fork, become session leader, setup syslog, clear environment variables,
-clear umask, and change directory to /. Drop privilages is the USER and 
-GROUP parameters are passed. Daemon will run as root if the USER and GROUP
-parameters are passed.
+Do required work to become a daemon as root. Forks a new process, becomes the session leader, sets up syslog, clears environment variables,
+clears umask, and changes directory to /.
+
+=head2 init_server_user(PIDFILE, USER, GROUP)
+
+Do required work to become a daemon as USER. Forks a new process, becomes the session leader, sets set syslog, clears environment variables,
+clears umask, and changes directory to /. Drops root privilages runs as user USER and group GROUP.
 
 =head2 log_debug
 
--- server.pl
+++ server.pl
@@ -7,53 +7,108 @@ use English qw( -no_match_vars );
 use IO::Socket;
 use NetFlow::Parser;
 use Getopt::Long;
-use Sys::Syslog;
+use SPM::Daemon qw( init_server_user log_die log_notice );
 use DBI;
 
-$SIG{'INT'} = 'INT_handler';
-my ($Port_Number);
+# Signal Handlers
+$SIG{'INT'}  = 'exit_handler';
+$SIG{'TERM'} = 'exit_handler';
 
-if (!defined $Port_Number) {
-    $Port_Number = 1234;
+# TODO: parse config file
+
+my ($Port);
+if (!defined $Port) {
+    $Port = 1234;
 }
 
-my $Database_File   = "netflow.db";
+my $Pid_File = '/var/run/server.pid';
+
+init_server_user($Pid_File, 'sean', 'sean');
+
+#my $Database_File = "/home/sean/projects/Perl-NetFlow-Collector/netflow.db";
+my $Database_File = "/home/sean/projects/Perl-NetFlow-Collector/netflow2.db";
+create_database($Database_File);
 my $Database_Handle = DBI->connect("dbi:SQLite:dbname=$Database_File", "", "", { AutoCommit => 1, RaiseError => 1 } );
+#create_db($Database_File);
+my $Exit = 0;
 
-my $sock = new IO::Socket::INET( LocalPort => $Port_Number, Proto => 'udp' )
-    or die "socket: $EVAL_ERROR";
-print "Waiting for UDP Packets on port $Port_Number\n";
-
-my ($MAXLEN, $data);
+my ($Maximum_Length, $data);
 # Maximum packet size for Netflow v5.
 # 24 + (30 * 48) = 1464 bytes
 # Header: 24 bytes
 # Each Flow: 48 bytes (can be 1 to 30 flows per packet)
 my $parser = NetFlow::Parser->new();
-$MAXLEN = 1464;
+$Maximum_Length = 1464;
 my $Packet;
 
 my $Header_Statement_Handle = $Database_Handle->prepare("INSERT INTO headers(version, count, system_uptime, unix_seconds, unix_nano_seconds, flow_sequence, engine_type, engine_id, sampling_mode, sampling_interval) VALUES (?,?,?,?,?,?,?,?,?,?)");
 my $Flow_Statement_Handle   = $Database_Handle->prepare("INSERT INTO flows(source_ip_address, destination_ip_address, next_hop_ip_address, snmp_input_index, snmp_output_index, packets, bytes, start_system_uptime, end_system_uptime, source_port, destination_port, tcp_flags, ip_protocol, type_of_service, source_system_number, destination_system_number, source_mask, destination_mask, header_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
 
 my $Header_Primary_Key;
-while ($sock->recv($data, $MAXLEN)) {
-    my ($port, $ip) = sockaddr_in($sock->peername);
+my $Sock;
+MAIN:
+while (!$Exit) {
+    if (!defined $Sock) {
+        $Sock = new IO::Socket::INET( LocalPort => $Port, Proto => 'udp' )
+            or die "socket: $EVAL_ERROR";
+        log_notice("listening on port $Port", "\n");
+    }
+
+    
+    if (!$Sock->recv($data, $Maximum_Length)) {
+        $Sock->close();
+        undef $Sock;
+        log_warn("UDP packet receive error: $OS_ERROR", "\n");
+        next MAIN;
+    }
+
+    my ($port, $ip) = sockaddr_in($Sock->peername);
     my $client = gethostbyaddr($ip, AF_INET);
     $Packet = $parser->parse($data);
-    print "Inserting into headers table\n";
+    log_notice("inserting into headers table", "\n");
     $Header_Statement_Handle->execute( $Packet->version(), $Packet->count(), $Packet->sys_uptime(), $Packet->unix_secs(), $Packet->unix_nsecs(), $Packet->flow_sequence(), $Packet->engine_type(), $Packet->engine_id, $Packet->sampling_mode, $Packet->sampling_interval() );
     $Header_Primary_Key = $Database_Handle->last_insert_id("", "", "", "");
     for my $flow (@{$Packet->flows()}) {
-        print "Inserting into flows table\n";
+        log_notice("inserting into flows table", "\n");
         $Flow_Statement_Handle->execute( $flow->srcaddr(), $flow->dstaddr(), $flow->nexthop(), $flow->input(), $flow->output(), $flow->packets(), $flow->bytes(), $flow->first(), $flow->last(), $flow->srcport(), $flow->dstport(), $flow->tcpflags(), $flow->protocol(), $flow->tos(), $flow->srcas(), $flow->dstas(), $flow->srcmask(), $flow->dstmask(), $Header_Primary_Key );
     }
 }
-die "recv: $OS_ERROR";
+clean_up();
+exit 0;
 
-sub INT_handler {
+sub clean_up {
+    log_notice("disconnecting from database $Database_File", "\n");
     $Database_Handle->disconnect;
-    print "Exiting\n";
-    exit 0;
+}
+
+# START
+# TODO: create database if it does not exist
+sub create_database {
+    my $database_file = shift;
+    # TODO: deal with undef db file
+    if (-f $database_file) {
+        return;
+        # TODO: check for valid db(can we connect?)
+    }
+
+    # create database tables
+    my $database_handle = DBI->connect("dbi:SQLite:dbname=$database_file", "", "", { AutoCommit => 1, RaiseError => 1 } );
+
+    my $create_headers_table = "CREATE TABLE IF NOT EXISTS headers ( header_id INTEGER NOT NULL PRIMARY KEY, version INTEGER NOT NULL, count INTEGER NOT NULL, system_uptime INTEGER NOT NULL, unix_seconds INTEGER NOT NULL, unix_nano_seconds INTEGER NOT NULL, flow_sequence INTEGER NOT NULL, engine_type INTEGER NOT NULL, engine_id INTEGER NOT NULL, sampling_mode INTEGER NOT NULL, sampling_interval INTEGER  NOT NULL )";
+    $database_handle->do($create_headers_table);
+
+    my $create_flows_table = "CREATE TABLE IF NOT EXISTS flows ( flow_id INTEGER NOT NULL PRIMARY KEY, source_ip_address TEXT NOT NULL, destination_ip_address TEXT NOT NULL, next_hop_ip_address TEXT NOT NULL, snmp_input_index INTEGER NOT NULL, snmp_output_index INTEGER NOT NULL, packets INTEGER NOT NULL, bytes INTEGER NOT NULL, start_system_uptime INTEGER NOT NULL, end_system_uptime INTEGER NOT NULL, source_port INTEGER NOT NULL, destination_port INTEGER NOT NULL, tcp_flags INTEGER  NOT NULL, ip_protocol INTEGER NOT NULL, type_of_service INTEGER NOT NULL, source_system_number INTEGER NOT NULL, destination_system_number INTEGER NOT NULL, source_mask INTEGER NOT NULL, destination_mask INTEGER NOT NULL, header_id INTEGER NOT NULL REFERENCES headers)";
+    $database_handle->do($create_flows_table);
+
+    # create database views
+    my $create_local_time_view = "CREATE VIEW IF NOT EXISTS headers_localtime AS SELECT header_id, version, count, system_uptime, datetime(unix_seconds, 'unixepoch', 'localtime'), unix_nano_seconds, flow_sequence, engine_type, engine_id, sampling_mode, sampling_interval FROM headers"; 
+    $database_handle->do($create_local_time_view);
+
+    $database_handle->disconnect;
+    return;
+}
+
+sub exit_handler {
+    $Exit = 1;
 }
 
