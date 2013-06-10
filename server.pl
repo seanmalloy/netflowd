19da2058ebde829feadb97771364f53fa1d894b5
Sean Malloy <spinelli85@gmail.com>
2013-06-09 23:22:13 -0500

    Update database creation code.
    Check for undefined argument. Check database connection. Check table
    and view names. Function create_database() now returns a DBI object.

--- server.pl
+++ server.pl
@@ -7,7 +7,7 @@ use English qw( -no_match_vars );
 use IO::Socket;
 use NetFlow::Parser;
 use Getopt::Long;
-use SPM::Daemon qw( init_server_user log_die log_notice );
+use SPM::Daemon qw( init_server_user log_die log_notice log_warn );
 use DBI;
 
 # Signal Handlers
@@ -26,10 +26,8 @@ my $Pid_File = '/var/run/server.pid';
 init_server_user($Pid_File, 'sean', 'sean');
 
 #my $Database_File = "/home/sean/projects/Perl-NetFlow-Collector/netflow.db";
-my $Database_File = "/home/sean/projects/Perl-NetFlow-Collector/netflow2.db";
-create_database($Database_File);
-my $Database_Handle = DBI->connect("dbi:SQLite:dbname=$Database_File", "", "", { AutoCommit => 1, RaiseError => 1 } );
-#create_db($Database_File);
+my $Database_File   = "/home/sean/projects/Perl-NetFlow-Collector/netflow2.db";
+my $Database_Handle = create_database($Database_File);
 my $Exit = 0;
 
 my ($Maximum_Length, $data);
@@ -81,31 +79,105 @@ sub clean_up {
     $Database_Handle->disconnect;
 }
 
-# START
-# TODO: create database if it does not exist
+# TODO: document this function
+# Returns: DBI database handle
 sub create_database {
     my $database_file = shift;
-    # TODO: deal with undef db file
-    if (-f $database_file) {
-        return;
-        # TODO: check for valid db(can we connect?)
+    if (!defined $database_file) {
+        log_die("Cannot connect to undefined database");
     }
 
-    # create database tables
+    if (-f $database_file) {
+        my $database_handle = DBI->connect("dbi:SQLite:dbname=$database_file", "", "", { AutoCommit => 1, RaiseError => 1 } );
+        if (!defined $database_handle) {
+            log_die("Cannot connect to database $database_file: $DBI::errstr");
+        }
+
+        # Exit if headers table does not exist.
+        if ( scalar(grep { $_ eq 'headers' } $database_handle->tables(undef, undef, 'headers', 'TABLE')) != 1) {
+            log_die("Missing table 'headers' in database $database_file");
+        }
+
+        # Exit if flows table does not exist.
+        if (scalar(grep { $_ eq 'flows' } $database_handle->tables(undef, undef, 'flows' , 'TABLE')) != 1) {
+            log_die("Missing table 'flows' in database $database_file");
+        }
+
+        # Exit if view does not exist.
+        if (scalar(grep { $_ eq 'headers_localtime' } $database_handle->tables(undef, undef, 'headers_localtime' , 'VIEW')) != 1) {
+            log_die("Missing view 'fheaders_localtime' in database $database_file");
+        }
+
+        return $database_handle;
+    }
+
+    # Create Database Tables
     my $database_handle = DBI->connect("dbi:SQLite:dbname=$database_file", "", "", { AutoCommit => 1, RaiseError => 1 } );
 
-    my $create_headers_table = "CREATE TABLE IF NOT EXISTS headers ( header_id INTEGER NOT NULL PRIMARY KEY, version INTEGER NOT NULL, count INTEGER NOT NULL, system_uptime INTEGER NOT NULL, unix_seconds INTEGER NOT NULL, unix_nano_seconds INTEGER NOT NULL, flow_sequence INTEGER NOT NULL, engine_type INTEGER NOT NULL, engine_id INTEGER NOT NULL, sampling_mode INTEGER NOT NULL, sampling_interval INTEGER  NOT NULL )";
-    $database_handle->do($create_headers_table);
+    my $create_headers_table_sql = <<'END_SQL';
+    -- Table for Netflow headers
+    CREATE TABLE IF NOT EXISTS headers (
+        header_id          INTEGER NOT NULL  PRIMARY KEY, -- Surrogate key
+        version            INTEGER NOT NULL,              -- Netflow version number
+        count              INTEGER NOT NULL,              -- Flows associated with header
+        system_uptime      INTEGER NOT NULL,              -- Milliseconds since the flow export device booted
+        unix_seconds       INTEGER NOT NULL,              -- Seconds since 0000 UTC 1970
+        unix_nano_seconds  INTEGER NOT NULL,              -- Residual nanoseconds since 0000 UTC 1970
+        flow_sequence      INTEGER NOT NULL,              -- Sequence counter of total flows seen
+        engine_type        INTEGER NOT NULL,              -- Type of flow-switching engine
+        engine_id          INTEGER NOT NULL,              -- Slot number of the flow-switching engine
+        sampling_mode      INTEGER NOT NULL,              -- Sampling mode
+        sampling_interval  INTEGER NOT NULL               -- Sampling interval
+    )
+END_SQL
+    $database_handle->do($create_headers_table_sql);
 
-    my $create_flows_table = "CREATE TABLE IF NOT EXISTS flows ( flow_id INTEGER NOT NULL PRIMARY KEY, source_ip_address TEXT NOT NULL, destination_ip_address TEXT NOT NULL, next_hop_ip_address TEXT NOT NULL, snmp_input_index INTEGER NOT NULL, snmp_output_index INTEGER NOT NULL, packets INTEGER NOT NULL, bytes INTEGER NOT NULL, start_system_uptime INTEGER NOT NULL, end_system_uptime INTEGER NOT NULL, source_port INTEGER NOT NULL, destination_port INTEGER NOT NULL, tcp_flags INTEGER  NOT NULL, ip_protocol INTEGER NOT NULL, type_of_service INTEGER NOT NULL, source_system_number INTEGER NOT NULL, destination_system_number INTEGER NOT NULL, source_mask INTEGER NOT NULL, destination_mask INTEGER NOT NULL, header_id INTEGER NOT NULL REFERENCES headers)";
-    $database_handle->do($create_flows_table);
+    my $create_flows_table_sql = <<'END_SQL';
+    -- Table for Netfow flows
+    CREATE TABLE IF NOT EXISTS flows (
+        flow_id                   INTEGER  NOT NULL  PRIMARY KEY,       -- Surrogate key
+        source_ip_address         TEXT     NOT NULL,                    -- Source IP address
+        destination_ip_address    TEXT     NOT NULL,                    -- Destination IP address
+        next_hop_ip_address       TEXT     NOT NULL,                    -- IP address of next hop router
+        snmp_input_index          INTEGER  NOT NULL,                    -- SNMP index of input interface
+        snmp_output_index         INTEGER  NOT NULL,                    -- SNMP index of output interface
+        packets                   INTEGER  NOT NULL,                    -- Packets in the flow
+        bytes                     INTEGER  NOT NULL,                    -- Total number of Layer 3 bytes in the packets of the flow
+        start_system_uptime       INTEGER  NOT NULL,                    -- System uptime at start of flow
+        end_system_uptime         INTEGER  NOT NULL,                    -- System uptime at the time the last packet of the flow was receive
+        source_port               INTEGER  NOT NULL,                    -- TCP/UDP source port number
+        destination_port          INTEGER  NOT NULL,                    -- TCP/UDP destination port number
+        tcp_flags                 INTEGER  NOT NULL,                    -- Cumulative OR of TCP flags
+        ip_protocol               INTEGER  NOT NULL,                    -- IP protocol type (for example, TCP = 6; UDP = 17)
+        type_of_service           INTEGER  NOT NULL,                    -- IP type of service (ToS)
+        source_system_number      INTEGER  NOT NULL,                    -- Autonomous system number of the source, either origin or peer
+        destination_system_number INTEGER  NOT NULL,                    -- Autonomous system number of the destination, either origin or pee
+        source_mask               INTEGER  NOT NULL,                    -- Source address prefix mask bits
+        destination_mask          INTEGER  NOT NULL,                    -- Destination address prefix mask bits
+        header_id                 INTEGER  NOT NULL  REFERENCES headers -- Setup one to many relationship between flows and headers
+    )
+END_SQL
+    $database_handle->do($create_flows_table_sql);
 
-    # create database views
-    my $create_local_time_view = "CREATE VIEW IF NOT EXISTS headers_localtime AS SELECT header_id, version, count, system_uptime, datetime(unix_seconds, 'unixepoch', 'localtime'), unix_nano_seconds, flow_sequence, engine_type, engine_id, sampling_mode, sampling_interval FROM headers"; 
-    $database_handle->do($create_local_time_view);
-
-    $database_handle->disconnect;
-    return;
+    # Create Database Views
+    my $create_local_time_view_sql = <<'END_SQL';
+    -- View for local time zone
+    CREATE VIEW IF NOT EXISTS headers_localtime AS
+    SELECT header_id,
+           version,
+           count,
+           system_uptime,
+           datetime(unix_seconds, 'unixepoch', 'localtime'),
+           unix_nano_seconds,
+           flow_sequence,
+           engine_type,
+           engine_id,
+           sampling_mode,
+           sampling_interval
+    FROM headers
+END_SQL
+    $database_handle->do($create_local_time_view_sql);
+    return $database_handle;
 }
 
 sub exit_handler {
